ede2807c19f32c3606064aa3bd43356e8ef0bc42
Syon Duke Abraham <kordan.goat@gmail.com>
2026-05-31 10:45:25 +0530

    Initialize data

--- Week 1 - Neural Networks/neurons_and_layers.py
+++ Week 1 - Neural Networks/neurons_and_layers.py
@@ -9,3 +9,23 @@ from tensorflow.python.keras.activations import sigmoid
 import logging
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
 
+# with this, TF supresses INFO messages and WARNING messages
+# and only displays ERROR messages
+
+# instead of:
+
+# INFO:tensorflow: Created device GPU:0
+# WARNING:tensorflow: Some operation may run slowly
+
+# which are just updates, you only get ERROR messages - actual errors and all the
+# informational messages and warning are supressed
+
+
+# neuron without activation - regression/linear model
+
+X_train = np.array([[1.0], [2.0]], dtype=np.float32)
+y_train = np.array([[300.0], [500.0]], dtype=np.float32)
+
+# data in TF is represented in a different way than what you've seen in NumPy
+# (as you know it)
+
