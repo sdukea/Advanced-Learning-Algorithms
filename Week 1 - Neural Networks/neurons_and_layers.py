65bd9d02a4145f888985704307b6bacb2dafb0f8
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-01 22:35:09 +0530

    Plot new classif. data

--- Week 1 - Neural Networks/neurons_and_layers.py
+++ Week 1 - Neural Networks/neurons_and_layers.py
@@ -269,3 +269,20 @@ Y_train = np.array([0,  0, 0, 1, 1, 1], dtype=np.float32).reshape(-1,1)  # 2-D M
 pos = Y_train == 1
 neg = Y_train == 0
 
+fig, ax = plt.subplots(1, 1, figsize=(4,3))
+
+ax.scatter(X_train[pos], Y_train[pos], marker='x', s=80, c='red', label='y=1')
+
+ax.scatter(X_train[neg], Y_train[neg], marker='o', s=100, label='y=0', facecolors=None, lw=3)
+
+ax.set_ylim(-0.08, 1.1)
+
+ax.set_ylabel('y', fontsize=12)
+
+ax.set_xlabel('x', fontsize=12)
+
+ax.set_title('One variable plot')
+
+ax.legend(fontsize=12)
+
+plt.show()
