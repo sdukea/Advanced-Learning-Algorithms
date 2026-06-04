5294c41c6097faaada5defbb3d68b3a09a0d1a0b
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-04 21:04:21 +0530

    Load data and interpret

--- Week 1 - Neural Networks/coffee_roasting_TF.py
+++ Week 1 - Neural Networks/coffee_roasting_TF.py
@@ -24,3 +24,15 @@ from lab_coffee_utils import load_coffee_data
 X, Y = load_coffee_data()
 
 print(X.shape, Y.shape)
+
+print(X)
+# shape: (200, 2)
+
+# so you have 200 training examples, each with 2 features - temperature of coffee beans (in celsius) 
+# and duration of roasting in minutes
+
+print(Y)
+# shape: (200,1)
+
+# this is for each tr. eg.: 1 for good roast, 0 for bad roast
+
