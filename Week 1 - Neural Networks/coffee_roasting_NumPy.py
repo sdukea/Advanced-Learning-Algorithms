ec25f8c59914559a79d6ed0eb5bc51787461744e
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-13 23:28:15 +0530

    Set temp. parameter values/layer

--- Week 1 - Neural Networks/coffee_roasting_NumPy.py
+++ Week 1 - Neural Networks/coffee_roasting_NumPy.py
@@ -146,3 +146,7 @@ def Sequential(x, W1, b1, W2, b2):
   
     return a2
 
+W1_tmp = np.array( [[-8.93,  0.29, 12.9 ], [-0.1,  -7.32, 10.81]] )
+b1_tmp = np.array( [-9.82, -9.28,  0.96] )
+W2_tmp = np.array( [[-31.18], [-27.59], [-32.56]] )
+b2_tmp = np.array( [15.41] )
