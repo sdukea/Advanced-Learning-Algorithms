71ae0ad0897f9e14cb0a9fd7ca11e5f9f7088275
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-13 23:31:58 +0530

    Deconstructing the parameters

--- Week 1 - Neural Networks/coffee_roasting_NumPy.py
+++ Week 1 - Neural Networks/coffee_roasting_NumPy.py
@@ -146,7 +146,19 @@ def Sequential(x, W1, b1, W2, b2):
   
     return a2
 
-W1_tmp = np.array( [[-8.93,  0.29, 12.9 ], [-0.1,  -7.32, 10.81]] )
-b1_tmp = np.array( [-9.82, -9.28,  0.96] )
+# layer 1 parameters
+W1_tmp = np.array( [[-8.93,  0.29, 12.9], [-0.1,  -7.32, 10.81]])
+b1_tmp = np.array( [-9.82, -9.28,  0.96])
+
+# so this means:
+# 1. we have two features
+# 2. we have three neurons
+
+# layer 2 parameters
 W2_tmp = np.array( [[-31.18], [-27.59], [-32.56]] )
 b2_tmp = np.array( [15.41] )
+
+# means:
+# 1. we have 3 features
+# 2. we have 1 neuron
+
