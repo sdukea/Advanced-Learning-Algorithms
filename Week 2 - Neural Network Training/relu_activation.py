9e76bf2e013457fb3fec31f7fc8da665bc4f735e
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-16 22:44:57 +0530

    Import all neccess.

---
+++ Week 2 - Neural Network Training/relu_activation.py
@@ -0,0 +1,7 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+import tensorflow as tf
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense, LeakyReLU
+from tensorflow.python.keras.activations import linear, relu, sigmoid
