9014c14e6e8c5d4604badd2948b216c975e14610
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-16 22:46:27 +0530

    Finish importing

--- Week 2 - Neural Network Training/relu_activation.py
+++ Week 2 - Neural Network Training/relu_activation.py
@@ -6,3 +6,6 @@ from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense, LeakyReLU
 from tensorflow.python.keras.activations import linear, relu, sigmoid
 
+from matplotlib.widgets import Slider
+import warnings
+warnings.simplefilter(action='ignore', category=UserWarning)
