5cff60440b4f6206d5dd11cab653030c4fd65d1d
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-17 09:42:41 +0530

    Move some

--- Week 2 - Neural Network Training/relu_activation.py
+++ Week 2 - Neural Network Training/relu_activation.py
@@ -9,3 +9,15 @@ from tensorflow.python.keras.activations import linear, relu, sigmoid
 from matplotlib.widgets import Slider
 import warnings
 warnings.simplefilter(action='ignore', category=UserWarning)
+
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+
+from lab_coffee_utils import load_coffee_data
+
+X, Y = load_coffee_data()
+
+print(X, Y)
+
+print(X.shape, Y.shape)
