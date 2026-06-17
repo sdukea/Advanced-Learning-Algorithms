e5a8a75ab26d3b9ecbcf3b97be993fbba3df3ea5
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-17 10:10:07 +0530

    Load; pre-normalization

--- Week 2 - Neural Network Training/relu_activation.py
+++ Week 2 - Neural Network Training/relu_activation.py
@@ -18,6 +18,17 @@ from lab_coffee_utils import load_coffee_data
 
 X, Y = load_coffee_data()
 
-print(X, Y)
-
 print(X.shape, Y.shape)
+
+# X -> (200, 2)
+# Y -> (200, 1)
+
+# 200 examples | 2 features
+# 200 binary labels | 1 column
+
+# normalize data
+
+print("Pre-normalization:")
+print(f"Temperature; max, min: {np.max(X[:, 0])},{np.min(X[:, 0])}")
+print(f"Duration; max, min: {np.max(X[:, 1])}, {np.min(X[:, 1])}")
+
