dab21caead8956f6f2b24b3cc54074c91f3f4eca
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-21 23:23:36 +0530

    Finish setting parameters; no backprop.

--- Week 2 - Neural Network Training/relu_activation.py
+++ Week 2 - Neural Network Training/relu_activation.py
@@ -234,3 +234,13 @@ def Sequential_(x, W1, b1, W2, b2):
 
 # set parameters
 
+W1_tmp = np.array( [[-8.93,  0.29, 12.9],  
+                    [-0.1,  -7.32, 10.81]]) 
+b1_tmp = np.array( [-9.82, -9.28,  0.96])  
+
+W2_tmp = np.array( [[-31.18],  
+                    [-27.59],   
+                    [-32.56]] ) 
+b2_tmp = np.array( [15.41] )   
+
+
