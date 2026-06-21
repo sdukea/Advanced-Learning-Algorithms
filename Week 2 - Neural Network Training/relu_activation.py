41f5010ab0bf786b1d2801fbca18889485ac920b
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-21 23:25:55 +0530

    Move on - softmax

--- Week 2 - Neural Network Training/relu_activation.py
+++ Week 2 - Neural Network Training/relu_activation.py
@@ -232,15 +232,10 @@ def Sequential_(x, W1, b1, W2, b2):
 
 # And Sequential just wraps it up - that's it!
 
-# set parameters
-
-W1_tmp = np.array( [[-8.93,  0.29, 12.9],  
-                    [-0.1,  -7.32, 10.81]]) 
-b1_tmp = np.array( [-9.82, -9.28,  0.96])  
-
-W2_tmp = np.array( [[-31.18],  
-                    [-27.59],   
-                    [-32.56]] ) 
-b2_tmp = np.array( [15.41] )   
-
+# 21/6/26, 11:24 PM
+# From here, we can set the parameters but will have to learn backpropogration for it
+# as activation function has changed and activations change and so have to use new parameters
+# for this and cannot copy from 'coffee_roasting_NumPy.py'
+# So, keeping the understanding of ReLU intact and moving on to Softmax implementation to then 
+# finish it and start backpropogation, its calculus and more.
 
