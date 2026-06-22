003e166357d0854383e0bb6bf24353083f46c6a4
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-22 22:37:49 +0530

    Import (2)

--- Week 2 - Neural Network Training/softmax.py
+++ Week 2 - Neural Network Training/softmax.py
@@ -1,3 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 plt.style.use('./deeplearning.mplstyle')
+import tensorflow as tf
+
+from tensorflow.python.keras.models import Sequential
+from tensorflow.python.keras.layers import Dense
+from sklearn.datasets import make_blobs
