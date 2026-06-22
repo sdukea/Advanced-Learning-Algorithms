81d42c727c225d3406f153167c76f013a35e8bdf
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-22 22:40:26 +0530

    Import (3) + logging/verbosity

--- Week 2 - Neural Network Training/softmax.py
+++ Week 2 - Neural Network Training/softmax.py
@@ -6,3 +6,8 @@ import tensorflow as tf
 from tensorflow.python.keras.models import Sequential
 from tensorflow.python.keras.layers import Dense
 from sklearn.datasets import make_blobs
+
+import logging
+logging.getLogger("tensorflow").setLevel(logging.ERROR)
+tf.autograph.set_verbosity(0)
+
