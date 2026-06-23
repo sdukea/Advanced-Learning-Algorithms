9a234d39196e41038dd7b5b922508bcfbd76b4cc
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-23 22:33:15 +0530

    Yield X- and y-train

--- Week 2 - Neural Network Training/softmax.py
+++ Week 2 - Neural Network Training/softmax.py
@@ -20,3 +20,5 @@ def my_softmax(z):
 # make datasets
 centers = [[-5, 2], [-2, -2], [1, 2], [5, -2]]
 
+X_train, y_train = make_blobs(n_samples=2000, centers=centers, cluster_std=1.0, random_state=30)
+
