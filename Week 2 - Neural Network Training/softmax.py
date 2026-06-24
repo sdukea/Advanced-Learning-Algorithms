9883fef6114acda80832869a7a42fc2c97f1d822
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-24 14:50:02 +0530

    Fix intuition + errors

--- Week 2 - Neural Network Training/softmax.py
+++ Week 2 - Neural Network Training/softmax.py
@@ -11,13 +11,17 @@ import logging
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
 tf.autograph.set_verbosity(0)
 
-def my_softmax(z):
-    ez = np.exp(z)              #element-wise exponenial
-    sm = ez/np.sum(ez)
+def my_softmax(z): # z -> an array of all z-values from neurons/units
+    ez = np.exp(z)              # ez -> an array where each z-value is exponentiated with base 'e'
+    sm = ez/np.sum(ez)          # sm -> an array where the 'ez' array is divided i.e. each value
+                                # in the array is divided with the value of np.sum(ez) which is a
+                                # single value
     return(sm)
 
 
 # make datasets
-centers = [[-5, 2], [-2, -2], [1, 2], [5, -2]]
+cent = [[-5, 2], [-2, -2], [1, 2], [5, -2]]
 
-X_train, y_train = make_blobs(n_samples=2000, centers=centers, cluster_std=1.0, random_state=30)
+X_train, y_train = make_blobs(n_samples=2000, centers=cent, cluster_std=1.0, random_state=30)
+
+
