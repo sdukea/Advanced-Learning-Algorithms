5119fc6f3c1ca59dff9aa52a9f40657a32dad956
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-27 09:24:29 +0530

    Initialize model

--- Week 2 - Neural Network Training/multiclass.py
+++ Week 2 - Neural Network Training/multiclass.py
@@ -26,3 +26,26 @@ X_train, y_train = cast(tuple[np.ndarray, np.ndarray], make_blobs(n_samples=m,
 
 print(X_train.shape, y_train.shape)
 
+
+# show unique classes
+print(f"Unique classes: {np.unique(y_train)}")
+
+# show class representation
+print(f"Class repr.: {y_train[:10]}")
+
+# model
+tf.random.set_seed(1234)
+
+model = tf.keras.Sequential([
+    tf.keras.layers.Dense(2, activation='relu', name='L1'),
+    tf.keras.layers.Dense(4, activation='linear', name='L2')
+])
+
+
+model.compile(
+    loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+    optimizer=tf.keras.optimizers.Adam(0.01)
+)
+
+model.fit(X_train, y_train, epochs=10)
+
