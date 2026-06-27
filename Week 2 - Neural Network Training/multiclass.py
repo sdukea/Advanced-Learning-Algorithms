7040e8dea3b3fb5430cc88e724c4bd4016e1526b
Syon Duke Abraham <kordan.goat@gmail.com>
2026-06-27 09:19:39 +0530

    Load data

--- Week 2 - Neural Network Training/multiclass.py
+++ Week 2 - Neural Network Training/multiclass.py
@@ -9,4 +9,20 @@ import logging
 logging.getLogger("tensorflow").setLevel(logging.ERROR)
 tf.autograph.set_verbosity(0)
 
+from typing import cast
+
+
+# 4-class dataset for classification
+classes = 4
+m = 100
+cent = np.array([[-5, 2], [-2, -2], [1, 2], [5, -2]])
+std = 1.0
+
+X_train, y_train = cast(tuple[np.ndarray, np.ndarray], make_blobs(n_samples=m,
+                                                             centers=cent,
+                                                             cluster_std=std,
+                                                             random_state=30))
+
+
+print(X_train.shape, y_train.shape)
 
